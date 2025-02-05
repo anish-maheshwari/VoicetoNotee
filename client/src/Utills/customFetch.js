220242d87736b2eb63716c14728911efa9d0677f
Anish <andyboy1jan@gmail.com>
2025-02-05 10:31:47 +0530

    customfetch

--- client/src/Utills/customFetch.js
+++ client/src/Utills/customFetch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const customFetch = axios.create({
   // baseURL: "http://localhost:5100/api/v1", // Update based on your backend
-  baseURL: "https://voicetonotee.onrender.com", // Update based on your backend
+  baseURL: "https://voicetonotee.onrender.com/api/v1", // Update based on your backend
 
   withCredentials: true, // Important: Ensures cookies are sent
   headers: {
