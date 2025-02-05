0ee2ae7fa524f4c96e7a5910c9438289ec569a3d
Anish <andyboy1jan@gmail.com>
2025-02-05 10:24:16 +0530

    base url changed for onrender

--- client/src/Utills/customFetch.js
+++ client/src/Utills/customFetch.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const customFetch = axios.create({
-  baseURL: "http://localhost:5100/api/v1", // Update based on your backend
+  // baseURL: "http://localhost:5100/api/v1", // Update based on your backend
+  baseURL: "https://voicetonotee.onrender.com", // Update based on your backend
+
   withCredentials: true, // Important: Ensures cookies are sent
   headers: {
     "Content-Type": "application/json",
