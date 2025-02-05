a134131def8e5e44421ff214a5fb0a33ec35ff19
Anish <andyboy1jan@gmail.com>
2025-02-05 10:25:04 +0530

    vite cofig.js removed prxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/api': {
-      target: 'http://localhost:5100/api',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
-    },
-  },
+ 
 })
